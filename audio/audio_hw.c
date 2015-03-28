5d8e07568ee80a0c561d5c20b894e16f8430773b
Sergiu Cozma <lssjbrolli@gmail.com>
2015-03-28 23:09:49 +0200

    klimtlte: mixer_paths closer to stock and small tweaks for our device
    Change-Id: Ic8caba881cfcc56a9481b7f6274cea68d1ba548e

--- audio/audio_hw.c
+++ audio/audio_hw.c
@@ -1892,7 +1892,7 @@ struct audio_module HAL_MODULE_INFO_SYM = {
         .module_api_version = AUDIO_MODULE_API_VERSION_0_1,
         .hal_api_version = HARDWARE_HAL_API_VERSION,
         .id = AUDIO_HARDWARE_MODULE_ID,
-        .name = "JA audio HW HAL",
+        .name = "KlimtLTE audio HW HAL",
         .author = "The CyanogenMod Project",
         .methods = &hal_module_methods,
     },
